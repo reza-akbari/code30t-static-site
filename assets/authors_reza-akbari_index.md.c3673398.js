7781d5f12c0e341f994f2643713b8b0cbf7664f5
Reza Akbari <co.alphacodes@gmail.com>
2021-09-12 14:36:49 +0430

    deploy

--- assets/authors_reza-akbari_index.md.c3673398.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as a}from"./app.846667f7.js";const e='{"title":"رضا اکبری","description":"","frontmatter":{"title":"رضا اکبری"},"relativePath":"authors/reza-akbari/index.md","lastUpdated":1630823081430}',r={};r.render=function(e,r,i,o,d,n){return t(),a("div")};export{e as __pageData,r as default};
